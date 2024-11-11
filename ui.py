91c6c23673caa800410d523b3bdce0810cd0376d
tambetos <tambet.osman@gmail.com>
2024-11-11 17:13:19 +0200

    Update ui.py
    yeaaah

--- ui.py
+++ ui.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter import messagebox
 from bs4 import BeautifulSoup
 import requests
-#oujee tambet
+#oujee randaru
+#siit tuleb midagi
 class ToidupoodRakendus:
     def __init__(self, root):
         self.root = root
